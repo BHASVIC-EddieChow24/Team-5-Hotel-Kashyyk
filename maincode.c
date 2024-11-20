067549e6ae8cc22f5963c4d4d9f9661d98b9b3d6
BHASVIC-charliebenson <charlie.benson24@students.bhasvic.ac.uk>
2024-11-20 09:34:23 +0000

    5

--- maincode.c
+++ maincode.c
@@ -9,9 +9,9 @@ int main(void) {
     rooms[3] = (75);
     rooms[4] = (75);
     rooms[5] = (50);
-    int age, adults[6], children[6], staylength[6], choice[6];
+    int dob,age, adults[6], children[6], staylength[6], choice[6];
     float mealtotal[6], roomtotal[6], total;
-    char dob[10],name1[6][13], name2[13], type[6], paper[6];
+    char name1[6][13], name2[6][13], type[6], paper[6];
     while(option == 4) {
         printf("What would you like to do?\nCheck in (1)\nBook a table (2)\nCheck out (3)\nQuit (4)\nAnswer:");
         fflush(stdin);
@@ -40,7 +40,7 @@ int main(void) {
                 printf("Enter your date of birth (ddmmyy):");
                 fflush(stdin);
                 scanf("%d", &dob);
-            }while(strlen(dob)>6);
+            }while(dob>1000000||dob<99999);
             do {
                 printf("Enter your age:");
                 fflush(stdin);
